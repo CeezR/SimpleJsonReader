037c6adfbe85e0b7e6afa61fcdb08703c460258b
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-19 10:42:48 +0200

    feat: simple write code implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,47 @@
 package org.example;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class Main {
+
+  public static Properties props = loadProperties();
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    //String FILE_LOCATION =  Application.props.getProperty("data.files.location");
+    Path demo = Paths.get(props.getProperty("data.files.location") + "/test.json");
+    if(!Files.exists(demo)) {
+      try {
+        Files.createFile(demo);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    try {
+      Files.write(demo, "Just created a JSON file!".getBytes(StandardCharsets.UTF_8));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    try {
+      System.out.println(Files.readAllLines(demo)); // -> [Just created a file!]
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  static Properties loadProperties() {
+    InputStream propertiesResource = Main.class.getClassLoader().getResourceAsStream("application.properties");
+    Properties props = new Properties();
+    try {
+      props.load(propertiesResource);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return props;
   }
 }
