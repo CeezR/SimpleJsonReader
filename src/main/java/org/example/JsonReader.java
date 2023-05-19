164f56c430072c79a453fb028a123490de799d59
Cesar Forthomme <cesar.forthomme@appliedtechnology.se>
2023-05-19 11:35:38 +0200

    feat: implemented json read logic in JsonReader

--- src/main/java/org/example/JsonReader.java
+++ src/main/java/org/example/JsonReader.java
@@ -1,10 +1,23 @@
 package org.example;
 
-import java.util.List;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JsonReader {
-
   public String readFile() {
-    return "";
+    Path path = Paths.get(Application.props.getProperty("data.files.location") + "/test.json");
+    StringBuilder sb = new StringBuilder();
+
+    if(Files.exists(path)) {
+      try {
+        Files.readAllLines(path).forEach(line -> sb.append(line));
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return sb.toString();
   }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,38 +10,7 @@ import java.util.Properties;
 
 public class Main {
 
-  public static Properties props = loadProperties();
-
   public static void main(String[] args) {
     //String FILE_LOCATION =  Application.props.getProperty("data.files.location");
-    Path demo = Paths.get(props.getProperty("data.files.location") + "/test.json");
-    if(!Files.exists(demo)) {
-      try {
-        Files.createFile(demo);
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    try {
-      Files.write(demo, "Just created a JSON file!".getBytes(StandardCharsets.UTF_8));
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    try {
-      System.out.println(Files.readAllLines(demo)); // -> [Just created a file!]
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  static Properties loadProperties() {
-    InputStream propertiesResource = Main.class.getClassLoader().getResourceAsStream("application.properties");
-    Properties props = new Properties();
-    try {
-      props.load(propertiesResource);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return props;
   }
 }
